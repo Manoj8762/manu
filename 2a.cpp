91163e222ca9bb8779b10ae781d1c866c1176114
Manoj8762 <161189764+Manoj8762@users.noreply.github.com>
2024-02-29 14:48:36 +0530

    Add files via upload

---
+++ 2a.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num [10], n;
+int i, j, man;
+cout<<"enter n for the numbers you want to sort"<<endl;
+cin>>n;
+	for(i=0;i<n;i++)
+{		
+			cout<<"enter number :"<<endl;
+	cin>>num[i];
+	}
+	for(i=0;i<n;i++)
+{
+		for(j=0;j<n;j++)
+{
+			if(num[i]<num[j])
+{
+				man=num[i];
+				num[i]=num[j];
+				num[j]=man;
+	}			
+	}
+	}
+	cout<<"ascending "<<endl;
+	for(i=0;i<n;i++)
+{
+	cout<<" "<<num[i]<<endl;
+}
+	for(i=0;i<n;i++)
+{
+		for(j=0;j<n;j++)
+{
+			if(num[i]>num[j])
+{
+				man=num[i];
+				num[i]=num[j];
+				num[j]=man;
+		}			
+		}
+	}
+	cout<<" descending"<<endl;
+	for(i=0;i<n;i++){
+		cout<<" "<<num[i]<<endl;
+}
+return 0;
+}
